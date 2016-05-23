4c0f75de4b795a9ea832d33b3d7dd0fe190f5d9c
avigail <avigailavraham7@gmail.com>
2016-05-23 17:11:12 +0300

    GREEN: test 3 passedgit status

--- src/map.js
+++ src/map.js
@@ -16,10 +16,30 @@ Map.prototype.find_a_person_location = function(name) {
 	return post.length!==0;
 };
 Map.prototype.check_map = function() {
-	var post=
-    this._posts.filter(
-    function( s ) { return s.indexOf() !== -1 && s.indexOf('at ') !== -1; });
-	return post.length==0;
+	var places=["Arad","Chabad house Bangkok","Langtang valley"];
+	var names=["avigail","Or A."];
+	var my_posts=[];
+	var posts=this._posts;
+	names.forEach(function(name) {
+	flag=false;
+	posts.forEach(function(post) {
+		if(post.indexOf(name)!== -1 && !flag)
+			flag=true;
+		else
+			my_posts+=post;	
+		});
+	});
+	/*my_posts.forEach(function(mypost) {
+	places.forEach(function(place) {
+		if(mypost.indexOf(place)!== -1)
+			count++;
+	if(count>1)
+		return false;
+		});
+	});*/
+if(my_posts.length==0)
+	return true;
+return false;
 };
   
 module.exports = Map;
